6f2f384f5ca77d6067a3d9ea4647006730f14e7a
Elvin <elvin.uchendu2020@gmail.com>
2022-10-17 20:38:58 -0700

    started .net application project

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+//Namespace
+namespace NumberGuesser
+{
+    //main Class
+    internal class Program
+    {
+        // Entry Point Method
+        public static void Main(string[] args)
+        {
+            //Set app vars
+            string appName = "Number Guesser";
+            string appVersion = "1.0.0";
+            string appAuthor = "Elvin Uchendu";
+            
+            // Change text color
+            Console.ForegroundColor = ConsoleColor.Green;
+            
+            //
+            Console.WriteLine("{0}: Version {1} by {2}", appName, appVersion, appAuthor);
+        }
+    }
+}
